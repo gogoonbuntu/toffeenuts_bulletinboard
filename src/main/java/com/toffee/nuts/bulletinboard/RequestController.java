d01d3deebe4b589e8aa599bd39ad85d89b23b9cb
Gogoonbuntu <34636395+gogoonbuntu@users.noreply.github.com>
2023-01-24 17:57:11 +0900

    ㅓㅓ

---
+++ src/main/java/com/toffee/nuts/bulletinboard/RequestController.java
@@ -0,0 +1,21 @@
+package com.toffee.nuts.bulletinboard;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RequestController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "Hello!");
+        return "hello";
+    }
+
+    @GetMapping("CreateNewBoard")
+    public String CreateNewBoard(Model model) {
+        model.addAttribute("data", "Hello!");
+        return "yes";
+    }
+}
